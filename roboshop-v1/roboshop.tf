b21cf0a1d2e032ef2a6902eea3574d18244e9d0f
lohitha <gorlelohitha@gmail.com>
2023-08-03 17:01:13 +0530

    terraform

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -11,6 +11,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z01549722JL2MSDGK9YRB"
+  name    = "frontend-dev.devopsb17.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
